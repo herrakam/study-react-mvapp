135660dbc3dafcd36cf9429a1247e2bc36cbb3ee
정승현 <fm1003@naver.com>
2020-07-30 18:16:58 +0900

    prop+ 오류 해결

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import React from 'react';
+import propTypes from "prop-types";
 
-function Food({ name }) {
-  return <h3>I love {name}</h3>
+function Food({ name, rating }) {
+  return <div>
+    <h3>I love {name}</h3>
+    <h3>my rating is {rating}/5.0 </h3>
+  </div>
 }
 
 const foodILike = [
-  { name: "kimchi" },
-  { name: "ramen" },
-  { name: "chicken" },
-  { name: "pizza" }
+  { name: "kimchi", rating: 5 ,id:1},
+  { name: "ramen", rating: 4.5,id:2 },
+  { name: "chicken", rating: 3.8,id:3 },
+  { name: "pizza", rating: 4.1,id:4 }
 ]
-function renderFood(dish) {
- 
-  return <Food name={dish.name} />
+
+Food.propTypes = {
+  name: propTypes.string.isRequired,
+  rating: propTypes.number
 
 }
 
 function App() {
   return (
     <div className="App">
-      {foodILike.map(renderFood)}
-
-
+      {foodILike.map((dish, index) => (
+        <Food
+          name={dish.name}
+          rating={dish.rating}
+          key = {index} />
+      ))}
     </div>
-
   );
 }
 
